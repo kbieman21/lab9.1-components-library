8f07ca6219b5e50c34ecb513754be793717a62d4
Kibreab Solomon <kbieman21@yahoo.com>
2025-10-30 00:01:16 -0700

    Added ProductDisplay component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 import Section from "./components/Section";
 import Button from "./components/Button";
@@ -6,32 +7,40 @@ import UserProfileCard from "./components/UserProfileCard";
 import ProductDisplay from "./components/ProductDisplay";
 import { log } from "console";
 
-
-
-
 function App() {
   const user = {
-  id: "1",
-  name: "John Doe",
-  email: "john.doe@example.com",
-  role: "Software Engineer",
-  avatarUrl: "https://example.com/avatar.jpg",
-};
+    id: "1",
+    name: "John Doe",
+    email: "john.doe@example.com",
+    role: "Software Engineer",
+    avatarUrl: "https://example.com/avatar.jpg",
+  };
 
-const product = {
-  id: "1",
-  name: "keyboard",
-  price: 10,
-  description: "gaming keyboard",
-  imageUrl: "https://images.unsplash.com/photo-1595044426077-d36d9236d54a?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1740",
-  inStock: true
-}
+  const product = {
+    id: "1",
+    name: "Wireless Headphones",
+    price: 199.99,
+    description: "High-quality wireless headphones with noise cancellation.",
+    imageUrl: "https://example.com/headphones.jpg",
+    inStock: true,
+  };
 
-// const addToCart = (productId:string) => {
-//   console.log(`Product Id# ${productId} add to cart`);
-//   alert(`Added ${product.name} to cart`);
-  
-// }
+  const [cartItems, setCartItems] = useState<string[]>([]);
+  const [showAlert, setShowAlert] = useState(true);
+
+
+  const handleAddToCart = (productId: string) => {
+    setCartItems([...cartItems, productId]);
+    console.log('Item added');    
+    // setShowAlert(true);
+    alert('Item added')
+  };
+
+  const handleOnClose = () => {  
+    setShowAlert(false);
+  };
+
+ 
   return (
     <main className="bg-white text-black h-screen p-5">
       <h1>Components Library</h1>
@@ -62,7 +71,7 @@ const product = {
             type="submit"
             onClick={() => console.log("Submit")}
           />
-          
+
           <Button
             text="Submit"
             type="reset"
@@ -74,17 +83,18 @@ const product = {
             disabled={false}
             onClick={() => console.log("disabled")}
           />
-       
         </div>
       </Section>
       <Section title="Lab9-AlertBox">
-        <AlertBox
+           <AlertBox
           type="success"
           message="We did it"
-          onClose={() => alert("Alert closed")}
+         onClose={() => alert("Alert closed")}
         >
           <p className="text-sm">You can now continue using the application.</p>
         </AlertBox>
+       
+       
         <AlertBox
           type="error"
           message="We did it"
@@ -106,25 +116,34 @@ const product = {
         >
           <p className="text-sm">You can now continue using the application.</p>
         </AlertBox>
+      </Section>
 
       <Section title="Lab9-UserProfileCard">
         <UserProfileCard
           user={user}
           showEmail={true}
           showRole={true}
-          onEdit={(id) => {console.log(`Editing userid# ${user.id}`);
-           alert(`Editing user ${id}`)}}
+          onEdit={(id) => {
+            console.log(`Editing userid# ${user.id}`);
+            alert(`Editing user ${id}`);
+          }}
         ></UserProfileCard>
       </Section>
-        
-      </Section>
 
-      <ProductDisplay product={product} 
-      showDescription={true} 
-      showStockStatus={true}  
-      onAddToCart={(productId) => {console.log(`Product with ID ${productId} added to cart.`);
-      }}>
-      </ProductDisplay>
+      <Section title="Lab9-ProductDisplay">
+        <ProductDisplay
+          product={product}
+          showDescription={true}
+          showStockStatus={true}
+          // onAddToCart={(productId) => {
+          //   alert(`Product with ID ${productId} added to cart.`);
+          // }}
+          
+          onAddToCart={handleAddToCart}
+        >
+          <div className="text-md text-gray-500">Free shipping available</div>
+        </ProductDisplay>
+      </Section>
     </main>
   );
 }
--- src/components/ProductDisplay.tsx
+++ src/components/ProductDisplay.tsx
@@ -21,23 +21,23 @@ function ProductDisplay({product, showDescription, showStockStatus, onAddToCart,
   return (
     
     <div>
-      <h1>Producs Detail</h1>
-      <p>Product Id: {product.id}</p>
-      <p>Product name: {product.name}</p>   
-      <p>Product price: {product.price}</p>     
-      {showDescription && <p>Description: {product.description}</p>}
-      <p>Image: {product.imageUrl}</p>
-      {showStockStatus && <p>Available: {product.inStock ? 'Yes' : 'No'}</p>}
+      <h1 className="text-4xl mb-5">Producs Detail</h1>
+        <p><img src="./wirelessHeadPhone.jpg" alt="product"  className="w-100 h-50 object-cover" /></p>
+      <p className="font-bold">Product Id: {product.id}</p>
+      <p  className="font-bold">Product name: {product.name}</p>   
+      <p  className="font-bold">Product price: {product.price}</p>     
+      {showDescription && <p  className="font-bold">Description: {product.description}</p>}
+    
+      {showStockStatus && <p  className="font-bold">Available: {product.inStock ? 'Yes' : 'No'}</p>}
       {onAddToCart && (
-        <button onClick={() => onAddToCart?.(product.id)}>
+        <button onClick={() => onAddToCart?.(product.id)}
+         className="px-4 py-2 bg-indigo-600 text-white text-sm rounded-md hover:bg-indigo-700 transition-colors duration-200">
         Add to Cart
       </button>
       )
 
       }
-      
-
-    <p>children</p>
+    {children}
       
     
 
